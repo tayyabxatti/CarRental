6fa500a5ffdc4317daa61635e57458d45418ed42
Tayyab Mushtaq <tayyabXatti@hotmail.com>
2019-02-19 01:50:18 +0500

    model and viewModel along with userControls

---
+++ CarRent/Model/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent.Model
+{
+    class Driver
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ CarRent/Model/RentDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent.Model
+{
+    class RentDb : DbContext
+    {
+        public RentDb() : base("name= DefaultConnection")
+        {
+
+        }
+        public DbSet<BasicCharges> BasicCharges { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<RentalAgreement> RentalAgreements { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
